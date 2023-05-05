f9f348b919797d8284b6419c0cf0b0af15934474
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-05-05 15:14:33 -0300

    Tratamento de exceções do update e atualizado o update no UserService

--- src/main/java/com/alexsander/course/services/UserService.java
+++ src/main/java/com/alexsander/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.alexsander.course.repositories.UserRepository;
 import com.alexsander.course.services.exceptions.DatabaseException;
 import com.alexsander.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
